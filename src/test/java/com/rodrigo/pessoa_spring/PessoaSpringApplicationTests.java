656b1a9f58f8b7ef6fbc0242f6b8f4877e5768e5
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-03-01 21:27:40 -0300

    Îniciando projeto CRUP pessoa endereco

---
+++ src/test/java/com/rodrigo/pessoa_spring/PessoaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.pessoa_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
