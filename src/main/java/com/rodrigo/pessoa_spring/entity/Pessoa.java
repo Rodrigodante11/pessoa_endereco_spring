7b9bd1bedf49c08ff4f55ed120bb299a971b2829
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-03-01 22:29:32 -0300

    Adicionando as Entitys Pessoa e Endereco

---
+++ src/main/java/com/rodrigo/pessoa_spring/entity/Pessoa.java
@@ -0,0 +1,30 @@
+package com.rodrigo.pessoa_spring.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table( name="pessoa" , schema = "pessoa_endereco")
+public class Pessoa {
+
+    @Id
+    @Column(name="id", unique = true)
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private Date data_nascimento;
+
+    @ManyToOne
+    @JoinColumn(name = "endereco_id")
+    private Endereco endereco;
+
+}
